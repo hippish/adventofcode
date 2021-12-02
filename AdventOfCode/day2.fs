50b2e822651ee0fcd11d226f76f4f0f9a40db70c
hippish <hippish@users.noreply.github.com>
2021-12-02 09:40:10 +0100

    day2 part2

--- AdventOfCode/day2.fs
+++ AdventOfCode/day2.fs
@@ -1,22 +1,40 @@
-type travelled = { depth: int; forwards: int }
+type travelled = { depth: int; forwards: int; aim: int }
 
 
 
-let forward x = (fun acc -> { forwards= acc.forwards + x; depth = acc.depth})
-let down x = (fun acc -> { forwards= acc.forwards; depth = acc.depth + x})
-let up x = (fun acc -> { forwards= acc.forwards; depth = acc.depth - x})
+let forward x =
+    (fun acc ->
+        { forwards = acc.forwards + x
+          depth = acc.depth + (x * acc.aim)
+          aim = acc.aim })
 
-let input = [forward 5;
-    down 5;
-    forward 8;
-    up 3;
-    down 8;
-    forward 2]
+let down x =
+    (fun acc ->
+        { forwards = acc.forwards
+          depth = acc.depth
+          aim = acc.aim + x })
+
+let up x =
+    (fun acc ->
+        { forwards = acc.forwards
+          depth = acc.depth
+          aim = acc.aim - x })
+
+let input =
+    [ forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2 ]
 
 let inputs = [|forward 9; forward 7; forward 4; down 7; forward 5; down 4; down 2; forward 1; down 6; forward 5; forward 5; forward 8; forward 3; forward 6; down 2; up 3; forward 1; up 4; forward 1; forward 6; up 2; forward 7; up 2; up 3; down 9; up 5; down 5; up 7; down 5; forward 4; forward 1; forward 8; forward 9; forward 3; forward 9; down 1; down 1; down 1; forward 6; up 8; down 2; forward 3; down 9; forward 7; down 5; up 7; down 3; forward 5; forward 9; down 9; up 3; forward 4; forward 8; up 2; forward 4; forward 2; forward 2; forward 8; up 7; up 4; down 3; forward 2; down 9; up 1; forward 2; down 3; forward 2; down 2; up 6; forward 6; forward 2; down 9; forward 3; forward 2; down 1; up 1; down 1; forward 5; forward 4; up 6; forward 6; forward 2; up 9; forward 9; forward 5; down 8; up 9; forward 2; up 3; forward 8; forward 2; down 4; down 1; up 9; up 8; forward 3; forward 9; down 9; down 6; forward 1; forward 5; up 9; down 7; up 9; down 8; down 2; down 2; up 9; forward 7; down 4; down 7; down 8; down 9; down 9; forward 9; down 9; forward 2; down 6; forward 8; forward 1; down 6; down 8; forward 5; forward 3; forward 5; down 5; forward 6; forward 3; forward 4; forward 3; down 2; down 9; down 8; down 9; up 7; up 9; up 1; down 6; down 4; forward 8; forward 5; forward 8; down 5; forward 7; down 9; forward 9; forward 1; up 4; down 4; forward 7; forward 4; up 9; forward 6; down 8; down 5; forward 8; down 6; down 3; down 7; forward 4; down 8; down 1; up 6; down 4; down 9; down 6; forward 7; down 8; forward 5; forward 8; down 7; down 5; forward 1; up 1; down 1; down 6; forward 5; forward 6; down 1; up 6; forward 4; forward 6; down 4; forward 3; up 6; forward 2; forward 9; down 8; forward 8; up 9; forward 3; forward 4; forward 1; down 4; down 5; forward 4; down 6; forward 7; down 7; down 8; up 8; up 5; down 8; down 5; forward 8; forward 2; forward 6; up 5; forward 4; forward 2; forward 1; up 7; forward 1; forward 6; down 8; down 5; down 4; forward 2; up 8; forward 4; up 8; forward 7; forward 5; down 4; up 7; down 5; down 3; forward 2; down 2; down 2; forward 5; forward 2; down 2; forward 5; down 8; forward 7; up 8; down 6; up 5; forward 6; up 9; down 2; down 3; up 1; up 8; forward 9; forward 7; forward 9; forward 3; down 2; up 2; down 2; down 8; up 8; up 6; forward 6; down 9; down 9; up 4; down 3; forward 6; forward 9; down 6; forward 7; forward 4; forward 4; down 9; down 3; forward 1; down 7; forward 2; forward 3; forward 9; forward 5; forward 2; forward 4; forward 8; up 1; forward 5; down 4; down 2; down 7; forward 1; up 1; up 8; up 6; down 1; forward 1; forward 9; forward 8; down 7; forward 6; forward 8; down 7; forward 5; down 5; down 8; down 8; forward 8; up 1; down 7; down 4; up 4; forward 5; up 7; forward 3; forward 2; down 1; forward 3; down 5; forward 4; down 4; forward 6; up 9; forward 3; down 7; forward 7; forward 9; forward 9; forward 4; up 9; up 5; down 6; down 6; forward 8; up 6; down 2; up 5; forward 7; forward 4; down 6; down 4; down 9; down 4; up 2; down 3; down 7; forward 1; forward 4; down 6; forward 3; forward 2; forward 4; down 9; forward 8; down 3; up 4; down 5; forward 2; down 6; forward 8; down 8; down 7; down 4; forward 1; down 3; forward 9; down 2; down 9; down 2; forward 1; down 3; down 2; down 2; up 4; down 8; forward 6; forward 4; forward 4; up 9; forward 3; forward 1; forward 1; up 3; forward 9; down 2; forward 5; down 9; down 2; forward 1; forward 9; down 3; forward 3; up 3; forward 7; down 6; up 8; down 2; down 5; forward 7; down 8; up 5; down 4; up 5; forward 6; forward 3; down 2; forward 4; forward 3; down 8; forward 5; forward 5; down 5; forward 1; forward 8; up 1; down 7; forward 6; forward 3; forward 8; down 9; down 7; forward 1; down 2; down 6; down 3; forward 8; down 7; forward 2; forward 1; forward 5; down 9; forward 2; forward 2; up 4; down 9; down 4; forward 7; down 7; up 8; forward 6; down 9; down 8; up 5; down 8; down 6; forward 9; up 5; up 7; down 3; up 2; down 4; up 8; up 3; down 7; forward 9; forward 7; down 7; forward 5; up 8; forward 1; down 2; forward 8; down 3; up 5; down 9; forward 8; down 7; down 3; down 3; down 2; forward 6; up 5; forward 4; down 4; down 3; down 5; forward 8; down 3; forward 7; forward 2; down 8; down 6; down 9; down 3; down 6; down 7; down 8; up 6; down 7; forward 8; down 9; forward 1; down 6; forward 8; down 5; forward 3; up 8; forward 1; down 6; forward 4; forward 5; forward 8; up 5; forward 4; down 2; down 9; up 2; forward 1; up 8; forward 6; up 4; up 6; forward 4; up 5; forward 6; forward 1; down 3; down 6; up 2; forward 4; up 2; forward 4; forward 6; down 2; down 4; up 5; down 9; up 2; down 4; up 6; forward 3; down 6; down 2; up 8; down 3; down 1; forward 6; forward 5; forward 8; down 4; down 6; down 2; forward 3; down 3; up 8; down 4; forward 5; down 6; down 3; up 2; forward 5; forward 2; down 6; down 8; forward 1; forward 5; forward 7; forward 3; forward 6; down 9; forward 7; forward 4; down 6; down 2; up 8; down 3; down 7; down 7; down 9; down 8; down 6; down 6; up 1; up 6; forward 4; down 8; up 7; down 8; forward 9; down 9; up 9; forward 4; forward 1; down 3; down 8; forward 9; down 9; forward 3; down 2; forward 9; down 2; forward 8; down 7; down 2; forward 4; forward 3; forward 3; down 8; up 3; forward 9; down 1; down 6; up 3; down 6; up 7; forward 9; up 9; down 5; forward 6; up 1; up 6; down 4; forward 9; forward 6; forward 9; down 4; up 9; up 4; forward 2; forward 2; forward 4; up 6; down 1; down 4; forward 9; down 9; forward 3; up 9; down 4; forward 4; down 1; forward 8; forward 2; down 1; down 7; down 8; forward 1; up 7; up 7; forward 1; down 3; up 5; down 4; forward 2; down 5; up 1; down 4; forward 7; down 2; down 5; down 4; forward 7; forward 6; up 9; forward 6; forward 1; forward 7; forward 5; up 6; down 8; forward 8; down 9; down 8; forward 8; down 2; down 5; forward 8; forward 9; down 6; down 3; down 3; up 9; down 6; forward 6; up 2; forward 9; forward 7; forward 6; forward 4; forward 1; down 2; forward 1; forward 3; forward 9; down 9; forward 7; forward 3; down 8; up 7; forward 1; down 8; up 5; down 8; up 3; down 7; forward 2; down 7; forward 2; down 3; forward 3; forward 8; down 4; forward 6; down 3; up 9; forward 9; up 6; up 4; up 6; down 1; forward 3; down 7; down 9; up 9; down 2; up 6; forward 4; down 4; down 3; down 2; down 6; forward 1; forward 1; up 3; forward 5; forward 8; down 1; up 4; forward 3; up 4; down 5; up 7; down 5; down 6; forward 9; forward 8; forward 9; down 6; forward 5; down 3; up 5; down 7; down 5; down 7; up 9; forward 3; forward 4; forward 1; up 3; forward 2; down 4; up 9; down 7; forward 6; forward 5; forward 3; forward 3; forward 9; up 7; down 9; forward 4; down 7; forward 9; forward 5; down 8; up 2; forward 2; down 4; up 5; up 4; forward 5; down 4; down 9; down 7; down 2; forward 1; forward 1; down 4; down 8; down 6; forward 1; up 6; up 3; up 5; down 1; down 5; up 1; up 5; forward 2; up 2; down 3; forward 7; forward 2; down 1; down 9; forward 1; down 1; forward 9; up 9; down 9; forward 9; down 4; down 1; up 5; down 2; forward 9; down 2; up 3; up 6; forward 1; forward 8; down 5; down 8; up 2; down 2; up 4; down 2; down 4; forward 6; up 4; down 1; forward 9; forward 4; down 9; up 7; forward 7; down 3; forward 2; down 6; up 6; down 5; down 7; forward 4; forward 1; forward 7; forward 4; forward 4; up 2; down 2; down 5; forward 7; down 6; forward 8; down 3; down 9; forward 7; forward 1; down 2; up 7; forward 4; forward 2; forward 6; forward 5; forward 9; forward 9; down 9; down 9; up 7; forward 7; forward 7; forward 1; forward 2; down 1; down 4; forward 7; forward 5; down 1; up 2; forward 3; forward 2; forward 1; forward 6; down 4; up 6; forward 7; down 1; forward 4; up 6; down 7; down 4; forward 1; down 8; down 2; down 1; down 8; forward 4; up 8; down 4; up 9; up 3; forward 6; up 9; down 1; forward 3; up 3; forward 5; up 3; down 6; forward 9; down 3; down 3; up 5; forward 5; forward 8; forward 9; down 6; down 3; forward 6; up 4; up 3; forward 3; forward 2; down 2; up 9; forward 4; forward 6; forward 2; up 9; down 2; forward 7; down 7; up 1; forward 2; forward 8; down 2; down 6; down 1; forward 3; forward 5; forward 6; forward 3; down 3; down 7; up 3; forward 2; forward 5; down 9; forward 3; down 9; up 6; down 6; forward 3; down 5; forward 1; down 5; up 3; forward 8; forward 8; down 5; down 6; down 1; forward 9; forward 4; forward 1; forward 8; down 8; down 9; forward 7; forward 9; down 2; down 6; down 8; down 3; forward 5; forward 7; forward 4; down 9; down 2; forward 4; forward 7; down 2; down 7; forward 8; down 8; forward 4; up 8; forward 3; forward 9; forward 4; down 9; down 6; up 1; down 3; down 7; down 4; forward 9; forward 4; up 9; down 6; forward 3; up 1; down 8; down 5; forward 9; down 4; down 2; down 2; down 5; up 5; down 5; forward 5; forward 2; up 1; forward 2; up 2; forward 8; down 2; down 7; forward 1|]
 
-let result = inputs |> Seq.fold (fun acc fn -> fn(acc)) {depth=0; forwards=0;}
 
-printfn "%A" result
+let result =
+    inputs
+    |> Seq.fold (fun acc fn -> fn (acc)) { depth = 0; forwards = 0; aim = 0 }
+
+printfn "result %A" result
 let final = result.depth * result.forwards
 printfn "%A" final
